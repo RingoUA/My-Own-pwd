5aad72e3b64febd847a277695631ba632fe74c55
Roman Mateichuk <roma.matei4uck@gmail.com>
2026-06-24 15:52:12 +0300

    First tests added for arg_parser

---
+++ tests/test_arg_parser.cpp
@@ -0,0 +1,90 @@
+#include <array>
+#include <string_view>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "arg_parser.hpp"
+#include "command.hpp"
+
+TEST_CASE("No arguments means logical mode")
+{
+    auto result = mopwd::parse_args(
+        std::array<std::string_view, 0>{}
+    );
+
+    REQUIRE(result);
+    REQUIRE(*result == mopwd::Command::LOGICAL);
+}
+
+TEST_CASE("-L means logical mode")
+{
+    auto result = mopwd::parse_args(
+        std::array{
+            std::string_view{"-L"}
+        }
+    );
+
+    REQUIRE(result);
+    REQUIRE(*result == mopwd::Command::LOGICAL);
+}
+
+TEST_CASE("--logical means logical mode")
+{
+    auto result = mopwd::parse_args(
+        std::array{
+            std::string_view{"--logical"}
+        }
+    );
+
+    REQUIRE(result);
+    REQUIRE(*result == mopwd::Command::LOGICAL);
+}
+
+TEST_CASE("-P means physical mode")
+{
+    auto result = mopwd::parse_args(
+        std::array{
+            std::string_view{"-P"}
+        }
+    );
+
+    REQUIRE(result);
+    REQUIRE(*result == mopwd::Command::PHYSICAL);
+}
+
+TEST_CASE("--physical means physical mode")
+{
+    auto result = mopwd::parse_args(
+        std::array{
+            std::string_view{"--physical"}
+        }
+    );
+
+    REQUIRE(result);
+    REQUIRE(*result == mopwd::Command::PHYSICAL);
+}
+
+TEST_CASE("Unknown option")
+{
+    auto result = mopwd::parse_args(
+        std::array{
+            std::string_view{"--unknown"}
+        }
+    );
+
+    REQUIRE_FALSE(result);
+    REQUIRE(result.error() == mopwd::ParseError::UNKNOWN_OPTION);
+}
+
+TEST_CASE("Too many arguments")
+{
+    auto result = mopwd::parse_args(
+        std::array{
+            std::string_view{"-P"},
+            std::string_view{"-L"}
+        }
+    );
+
+    REQUIRE_FALSE(result);
+    REQUIRE(result.error() == mopwd::ParseError::TOO_MANY_ARGUMENTS);
+}
